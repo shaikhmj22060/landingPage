dbd8c3b3ab7ac7ad124e776ebf9ec31ea6ae48b7
shaikhmj22060 <shaikhm.ja.z@gmail.com>
2026-04-01 12:36:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-neutral-950 text-white h-screen flex items-center justify-center text-4xl'>
+      Hello from the app component!
+    </div>
+  )
+}
+
+export default App
