207ee4c9aa22f46742a9275695bc4375e7e6f36e
shaikhmj22060 <shaikhm.ja.z@gmail.com>
2026-04-06 15:40:32 +0530

    heroPage component added to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import ReactLenis from "lenis/react";
+import {
+  useScroll,
+  useTransform,
+  motion,
+  useMotionValueEvent,
+} from "motion/react";
+import { useRef } from "react";
+import HeroPage from "./Pages/HeroPage";
 
 const App = () => {
-  return (
-    <div className='bg-neutral-950 text-white h-screen flex items-center justify-center text-4xl'>
-      Hello from the app component!
-    </div>
-  )
-}
+  // const ref = useRef(null);
+  // const { scrollYProgress } = useScroll({
+  //   target: ref,
+  //   offset: ["start start", "end end"],
+  // });
+  // useMotionValueEvent(scrollYProgress, "change", (latest) => {
+  //   console.log("Page scroll: ", latest);
+  // });
+  // const scale = useTransform(scrollYProgress, [0, 0.5, 1], [1, 0.8, 1]);
+  // const scaleMiddle = useTransform(scrollYProgress, [0, 0.5, 1], [1, 1, 0.8]);
 
-export default App
+  return (
+    <ReactLenis root options={{ smoothWheel: true }}>
+      <motion.div className="relative h-[300vh] bg-white">
+        <motion.div className="">
+          <HeroPage />
+        </motion.div>
+      </motion.div>
+    </ReactLenis>
+  );
+};
+
+export default App;
